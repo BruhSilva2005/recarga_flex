aa7a2adc11a35a843532d481c4e49dbe161fbc8e
bruna <bruna.ssantos102@senacsp.edu.br>
2023-11-14 16:19:35 -0300

    14-11

--- admin/redirecionamentos/guia_usuario.php
+++ admin/redirecionamentos/guia_usuario.php
@@ -1,47 +1,47 @@
 <h1>Ajuda</h1>
     <h2>Sumário</h2>
     <ul>
-        <ol> <a href="#introducao">1. INTRODUÇÃO .................................................................................................................................................................... 4 </a></ol>
+        <ol> <a href="#introducao">1. INTRODUÇÃO ______________________________________________________________________________4 </a></ol>
     </ul>
     <lu>
-        <ol><a href="#publico">2. PÚBLICO ALVO ........................................................................................................................................... 5
+        <ol><a href="#publico">2. PÚBLICO ALVO __________________________________________________________________________________ 5
         </a></ol>
     </lu>
     <lu>
-        <ol><a href="#acesso">3. ACESSO........................................................................................................................................... 6
+        <ol><a href="#acesso">3. ACESSO_________________________________________________________________________________________   6
         </a></ol>
     </lu>
     <lu>
-        <ol>4.1 Cadastro de Usuário....................................................................................................................................................6
+        <ol>4.1 Cadastro de Usuário..............................................................................................................................................................6
         </ol>
     </lu>
     <lu>
-        <ol>4.2 Cadastro do Motorista................................................................................................................................................... 6
+        <ol>4.2 Cadastro do Motorista......................................................................................................................................................... 6
         </ol>
     </lu>
     <lu>
-        <ol>4.3 Cadastro do Estudante................................................................................................................................................... 6
+        <ol>4.3 Cadastro do Estudante....................................................................................................................................................... 6
         </ol>
     </lu>
     <lu>
-        <ol><a href="#acesso">5. ACESSO AO MENU ................................................................................................................................................... 10
+        <ol><a href="#acesso">5. ACESSO AO MENU ____________________________________________________________________________ 10
         </a></ol>
     </lu>
     <lu>
-        <ol><a href="#recarga">6. RECARGA DO CARTÃO ................................................................................................................................. 11
+        <ol><a href="#recarga">6. RECARGA DO CARTÃO _________________________________________________________________________ 11
 </a> </ol>
     </lu>
     <lu>
-        <ol><a href="#consuta">6.1 CONSULTA DE SALDO ................................................................................................................................................... 12
+        <ol><a href="#consulta">6.1 CONSULTA DE SALDO _________________________________________________________________________  12
         </a></ol>
     </lu>
     <lu>
-        <ol>6.2 BLOQUEIO DO CARTÃO................................................................................................................................................... 13
-        </ol>
+        <ol><a href="#bloqueio">6.2 BLOQUEIO DO CARTÃO_________________________________________________________________________  13
+        </a></ol>
     </lu>
     <lu>
-        <ol>6.3 DISQUE DENÚNCIA................................................................................................................................ 16
-        </ol>
+        <ol><a href="#bloqueio">6.3 DISQUE DENÚNCIA_____________________________________________________________________________  16
+        </a></ol>
     </lu>
 
     <h2 id="introducao">1.INTRODUÇÃO</h2>
@@ -82,7 +82,7 @@
     5.1-Caso alguma infromação preenchida incorreto, Mensagem "Erro ao Cadastrar".<br>
     5.2-Refazer passo a passo.
     
-<h3 id="menu">3.3 CADASTRO DE USUÁRIO</h3>
+<h3 id="menu">4 CADASTRO DE USUÁRIO</h3>
 <p>Para poder cadastrar um usuario no sistema precisa seguir os seguintes passo:<br>
     1-No menu inicial localize o Cabeçalho(Parte Superior), Click em "Usuario".<br>
     2-Aberto o Menu de usuario, click em "Cadastrar".<br>
@@ -92,9 +92,9 @@
     5.1-Caso alguma infromação preenchida incorreto, Mensagem "Erro ao Cadastrar".<br>
     5.2-Refazer passo a passo.
 </p>
-<h3 id="recarga">4. RECARGA do CARTÃO</h3>
+<h3 id="recarga">5.1 RECARGA do CARTÃO</h3>
 <p>Para poder recargar o cartão no sistema precisa seguir os seguintes passo:<br>
-1-No menu inicial localize o botaõ de recarga(Parte Superior), Click em "Recarregar".<br>
+1-No menu inicial localize o botão de recarga(Parte Superior), Click em "Recarregar".<br>
     2-Aberto o Menu de usuario, click em "Recarregar".<br>
     3-Em tela de Recarregar, Preencher as informações pedida.<br>
     4-Apos preenchimento, Click em pagar.<br>
@@ -102,7 +102,29 @@
     5-Visualizar Mensagem de "Pagamento efetuado com Sucesso".<br>
     5.1 caso não consiga, click no botão de ajuda
     5.2-Refazer passo a passo.
-    <h3 id="consulta">5. RECARGA do CARTÃO</h3>
+    <h3 id="consulta">5.4 CONSULTA DE SALDO</h3>
+    <P>Para poder saber o quanto de saldo ainda resta no seu cartão , você precisa seguir os seguintes passo-a-passo:<br>
+    1-No menu inicial localize o botão de consulta de saldo(Parte Superior), Click em "consultar saldo".<br>
+    2-Aberto o MENU  de saldo, click em "VER SALDO".<br>
+    3-Nessa tela você conseguirá Visualizar o saldo do seu cartão.<br>
+    4-Caso queira Recarregar click no botão "Recarregar CARTÃO".<br>
+    5-caso não consiga, click no botão de ajuda<br>
+    5.1-Refazer passo a passo de "recarga de saldo".<br>
+    <h3 id="bloqueio">5.3 BLOQUEIO DO CARTÃO</h3>
+    <P>O bloqueio de cartão consiste em bloquear se cartão em caso de perda, siga o passo-a-passo:<br>
+    1-No menu inicial localize o botão de Bloqueio de cartão(Parte Superior), Click em "bloquear".<br>
+    2- Apos aberto o MENU  de bloqueio.<br>
+    3- Será pedido seu cpf, e tera a opcão de cartão comun,cartão de Estudante.<br>
+    4- apos a sua escolha você vizualizará a Mensagem"cartão bloqueado".<br>
+    5- caso não consiga, volte para a pagina principal e click em Ajuda.<br>
+    5- refaça o passo-a-passo.<br>
+    <h3 id="disque">5.4 DISQUE DENÚNCIA</h3>
+    <P> Para utilizar o serviço disque-denuncia siga o passo-a-passo<br>
+    1-No menu inicial localize o botão de Fazer denúncia(Parte Superior).<br>
+    2- Apos aberto a pagina de "BLOQUEIO".<br>
+    3- tera uma barra de escrita para voce colocar sua denuncia.<br>
+    4- logo apos click em "enviar".<br>
+    4-E então analizaremos sua denuncia e tomaremos as devidas ações.<br>
 
 
             
